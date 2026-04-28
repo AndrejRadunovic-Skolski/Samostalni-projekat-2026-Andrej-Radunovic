aae090b50d52c51b0cfb6a53837f8fbb9d05b67f
AndrejRadunovic-Skolski <andrej.radunovic@prvabeogim.edu.rs>
2026-04-28 11:16:38 +0200

    RADI GLEDANJE POSTOVA

--- Glavna.cs
+++ Glavna.cs
@@ -25,6 +25,8 @@ namespace Samostalni_projekat_2026_Andrej_Radunovic
             DataTable tabela = new DataTable();
             adapter.Fill(tabela);
             ComboKategorija.DataSource = tabela;
+
+            ucitajObjave(1);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -36,17 +38,10 @@ namespace Samostalni_projekat_2026_Andrej_Radunovic
         void ucitajObjave(int Kategorija)
         {
             SqlConnection veza = Konekcija.NapraviVezu();
-            string query = @"SELECT 
-                            o.objava_id,
-                            o.naslov, 
-                            o.sadrzaj, 
-                            k.ime AS Autor,
-                            ISNULL(SUM(g.vrednost), 0) AS UkupnoGlasova
-                            FROM Objava o
-                            INNER JOIN Korisnik k ON o.korisnik_id = k.korisnik_id
-                            LEFT JOIN Glas g ON o.objava_id = g.objava_id
-                            GROUP BY o.objava_id, o.naslov, o.sadrzaj, k.ime, o.datum_objave
-                            ORDER BY o.datum_objave DESC;";   
+            string query = @"SELECT o.naslov, o.sadrzaj, k.ime AS Autor 
+                     FROM Objava o 
+                     INNER JOIN Korisnik k ON o.korisnik_id = k.korisnik_id 
+                     WHERE o.glavna = 1";
             SqlCommand cmd = new SqlCommand(query, veza);
             veza.Open();
             SqlDataReader reader = cmd.ExecuteReader();
@@ -58,7 +53,7 @@ namespace Samostalni_projekat_2026_Andrej_Radunovic
                 post.PromeniPodatke(
                     reader["naslov"].ToString(),
                     reader["Autor"].ToString(),
-                    reader["UkupnoGlasova"].ToString()
+                    "NaN"
                 );
 
                 flowLayoutPanel1.Controls.Add(post);
